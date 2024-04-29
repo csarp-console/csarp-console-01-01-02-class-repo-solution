80dea8cbc9dbcf7a21bc1f477a50a54e7e1f9fb8
Csaba Gyuris <gyuris.csaba@vasvari.org>
2024-04-29 17:08:35 +0200

    Elkezdve a megoldás

---
+++ Kreta.Console/Models/SchoolCitizens/AwardedStudent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta.Console.Models.SchoolCitizens
+{
+    public class AwardedStudent
+    {
+        public AwardedStudent(string name, int age, int scholarship, bool montlyScholarchip)
+        {
+            Name = name;
+            Age = age;
+            Scholarship = scholarship;
+            MontlyScholarchip = montlyScholarchip;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Scholarship { get; set; }
+        public bool MontlyScholarchip { get; set; }
+
+        public bool IsAdult => Age > 18;
+
+        public static bool IsYounger(AwardedStudent student1, AwardedStudent student2)
+        {
+            if (student1.Age < student2.Age)
+                return true;
+            else
+                return false;
+        }
+        public override string ToString()
+        {
+            string montly = "Az ösztöndíj egyszeri juttatás!";
+            if (MontlyScholarchip)
+                montly = "Az ösztöndíj havi juttatás!";                
+            return $"{Name} {Age} éves és {Scholarship} Ft ösztöndíjat nyert! {montly}";
+        }
+
+    }
+}
---
+++ Kreta.ConsoleTests/Models/SchoolCitizens/AwardedStudentTestsIsYounger.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kreta.Console.Models.SchoolCitizens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kreta.Console.Models.SchoolCitizens.Tests
+{
+    [TestClass()]
+    public class AwardedStudentTestsIsYounger
+    {
+        [TestMethod()]
+        public void IsYoungerTestFirstIsJunger()
+        {
+            AwardedStudent student1 = new AwardedStudent("Üveges Tünde", 17, 15000, false);
+            AwardedStudent student2 = new AwardedStudent("Dolgozo Dénes", 18, 5000, true);
+            
+            bool acutal = AwardedStudent.IsYounger(student1, student2);
+
+            Assert.IsTrue(acutal);
+        }
+
+        [TestMethod()]
+        public void IsYoungerTestLastISJunger()
+        {
+            AwardedStudent student1 = new AwardedStudent("Üveges Tünde", 17, 15000, false);
+            AwardedStudent student2 = new AwardedStudent("Dolgozo Dénes", 15, 5000, true);
+
+            bool acutal = AwardedStudent.IsYounger(student1, student2);
+
+            Assert.IsFalse(acutal);
+        }
+
+        [TestMethod()]
+        public void IsYoungerTestBornInSameYear()
+        {
+            AwardedStudent student1 = new AwardedStudent("Üveges Tünde", 17, 15000, false);
+            AwardedStudent student2 = new AwardedStudent("Dolgozo Dénes", 17, 5000, true);
+
+            bool acutal = AwardedStudent.IsYounger(student1, student2);
+
+            Assert.IsFalse(acutal);
+        }
+    }
+}
